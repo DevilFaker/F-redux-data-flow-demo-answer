fae8fc7597f5a88d0607a52559dffdcf01ef61e3
kevindongzg <dongkevin94@gmail.com>
2020-05-31 20:45:51 +0800

    update

--- src/reducers/name.js
+++ src/reducers/name.js
@@ -8,7 +8,7 @@ const name = (state = 'Default Name', action) => {
 // const name = (state = { firstName: 'Default', lastName: 'Name' }, action) => {
 //   if (action.type === 'SET_NAME') {
 //     return Object.assign({}, state, action.payload);
-//     // return Object.assign(...state, action.payload);
+//     // return {...state, ...action.payload};
 //   }
 //   return state;
 // };
